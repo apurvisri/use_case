a97b232ff9d6dd8d6a9026484721eae0e0afe68b
Apurvi Srivastava <apurvisrivastava@Apurvis-MacBook-Air.local>
2021-04-15 23:18:44 +0530

    updated script with comments

--- multiple_script.sh
+++ multiple_script.sh
@@ -2,12 +2,16 @@
 
 VERSION="/Users/apurvisrivastava/Documents/Version/version.txt"
 
+#Looking for version number in version.txt file
 if [ -f "$VERSION" ];
 then 
     SUBSTRING=`cut -d' ' -f 3 $VERSION`
 #echo $SUBSTRING
+else
+echo "Check version.txt file again!!"
 fi
 
+# Adding file numeric pattern of shell script in array  
 Script_Path="/Users/apurvisrivastava/Desktop/Apurvi/BashScripts/"
 declare -a arr=() 
 for name in $Script_Path/*sh;
@@ -15,18 +19,20 @@ do
 arr+=( "$(sed -e s/[^0-9]//g $name)" )
 done
 
+#Itrating array and comparing it with version in version.txt file
 for i in "${arr[@]}";
 do 
 if [[ $i -gt $SUBSTRING ]];
 then 
 sh $Script_Path/$i.*.sh
 sum=`expr $i + 1`
+else
+echo "$i: conditions are not valid"
 fi
 done
 arr=()
 
-#`sed -i '' 's/${SUBSTRING}/${sum}/g' /Users/apurvisrivastava/Documents/Version/version.txt`
-
+#Incrementing version in version.txt file as per shell script numeric pattern.
 path="/Users/apurvisrivastava/Documents/Version/backup/"
 TFILE="/Users/apurvisrivastava/Documents/Version/backup/tmp"
 for f in $VERSION
@@ -35,6 +41,8 @@ do
 then
 cp -f $f $path
 sed "s/$SUBSTRING/$sum/g" "$f" > $TFILE && mv $TFILE "$f"
+command=`cat $f`
+echo "Latest: $command in version.txt file"
 else
 echo "Error: Cannot read $f"
 fi
